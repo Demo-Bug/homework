70278ec2cb8f4d079d95f0c79c025e0e1bbf0624
Jie lu <1298078630@qq.com>
2017-09-08 21:22:28 +0800

    'js'

---
+++ JavaScript/9-4/index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: Administrator
+* @Date:   2017-09-04 11:02:20
+* @Last Modified by:   Administrator
+* @Last Modified time: 2017-09-04 11:21:00
+*/
+alert('这是外部JS文件');
+console.log(11);
+document.write("aaa");
+document.write('<h1>我是</h1>');
